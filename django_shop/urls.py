af3c4a1175c18bab31580cc2f9742fdbf03b0c59
majahed228 <pluzhnikov93@inbox.ru>
2022-05-25 00:30:59 +0300

    added admins

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from products.views import index, products
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products'),
 ]
--- products/models.py
+++ products/models.py
@@ -17,3 +17,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
+
