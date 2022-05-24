f236b8deffe48578403340654ec908e371f10c68
majahed228 <pluzhnikov93@inbox.ru>
2022-05-24 23:32:56 +0300

    added model product category

--- products/views.py
+++ products/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(requests):
     return render(requests, 'products/index.html')
+
+
+def products(requests):
+    return render(requests, 'products/product.html')
